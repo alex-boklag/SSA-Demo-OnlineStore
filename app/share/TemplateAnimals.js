9e3e47d6fd74e75a30488778acf753d5b45ae75e
alexboklag <sanya32102@gmail.com>
2019-12-18 19:27:57 +0200

    Add TemplateAnimals

---
+++ app/share/TemplateAnimals.js
@@ -0,0 +1,26 @@
+export class TemplateAnimals {
+  getAnimalsTemplate({ image, price, breed, gender, birth_date, weight }) {
+    const months = Math.floor((Date.now() - new Date(birth_date)) / 2592000000);
+
+    return `<div class="uk-margin uk-width-medium@s">
+    <div class="uk-card uk-card-default uk-card-hover">
+      <div class="uk-card-media-top">
+        <img src="${image}" alt="${breed}">
+      </div>
+      <div class="uk-card-header uk-padding-small">
+        <p class="uk-text-large uk-text-bold uk-text-center uk-margin-small">${breed}</p>
+        <p class="uk-text-bold uk-text-center uk-margin-small">$${price}</p>
+      </div>
+      <div class="uk-card-body uk-text-center uk-padding-small">
+        <p class="uk-text-bold">
+          <span class="uk-text-small">${gender} / ${months} months / ${weight} kg</span>
+        </p>
+      </div>
+      <div class="uk-card-footer uk-text-center uk-padding-small">
+        <button class="uk-button button__buy">Buy</button>
+        <button class="uk-button button__details">Details</button>
+      </div>
+    </div>
+  </div>`;
+  }
+}
