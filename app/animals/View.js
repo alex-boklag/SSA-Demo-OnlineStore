d98e5a279aa55d8b353bb53b0f488cd44056195e
alexboklag <sanya32102@gmail.com>
2019-12-19 11:26:58 +0200

    Add method for adding listeners

--- app/animals/View.js
+++ app/animals/View.js
@@ -6,11 +6,26 @@ export class View {
     this.templater = new TemplateAnimals();
   }
 
-  renderAnimals(animals) {
+  addListeners(handleClickPrevPage, handleClickNextPage, handleClickPageNumber) {
+    const btnPrevPage = document.querySelector('.prev-page');
+    const btnNextPage = document.querySelector('.next-page');
+    const btnPageNumbers = [...document.querySelectorAll('[class*="page-"]')];
+
+    btnPrevPage.addEventListener('click', handleClickPrevPage);
+    btnNextPage.addEventListener('click', handleClickNextPage);
+    btnPageNumbers.forEach(item => item.addEventListener('click', handleClickPageNumber));
+
+    document.addEventListener('keypress', event => (event.key === 'a') ? handleClickPrevPage() : null);
+    document.addEventListener('keypress', event => (event.key === 'd') ? handleClickNextPage() : null);
+  }
+
+  renderAnimals(animals, offset, amount) {
     let newsStr = '';
 
-    animals.forEach(animal => {
-      newsStr += this.prepareAnimalCard(animal);
+    animals.forEach((animal, index) => {
+      if (index >= offset && index < offset + amount) {
+        newsStr += this.prepareAnimalCard(animal);
+      }
     });
 
     this.domAnimals.innerHTML = newsStr;
