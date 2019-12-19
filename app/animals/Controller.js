f81a711196b8ec830c083ac5da23978b4c398242
alexboklag <sanya32102@gmail.com>
2019-12-19 11:26:10 +0200

    Add handlers for pagination

--- app/animals/Controller.js
+++ app/animals/Controller.js
@@ -5,11 +5,34 @@ export class Controller {
   constructor() {
     this.model = new Model();
     this.view = new View();
-    this.showAnimals();
+    this.view.addListeners(this.handleClickPrevPage.bind(this), this.handleClickNextPage.bind(this), this.handleClickPageNumber.bind(this), );
+    this.offset = 0;
+    this.amount = 20;
+    this.showAnimals(this.offset, this.amount);
   }
 
-  showAnimals() {
+  handleClickPrevPage() {
+    if (this.offset - this.amount >= 0) {
+      this.offset -= this.amount;
+      this.showAnimals(this.offset, this.amount);
+    }
+  }
+
+  handleClickNextPage() {
+    if (this.offset + this.amount < 320) {
+      this.offset += this.amount;
+      this.showAnimals(this.offset, this.amount);
+    }
+  }
+
+  handleClickPageNumber(ev) {
+    const pageNumber = Number(ev.target.innerText);
+    this.offset = pageNumber * this.amount - this.amount;
+    this.showAnimals(this.offset, this.amount);
+  }
+
+  showAnimals(offset, amount) {
     this.model.getAnimals()
-      .then(obj => this.view.renderAnimals(obj.animals));
+      .then(obj => this.view.renderAnimals(obj.animals, offset, amount));
   }
 }
