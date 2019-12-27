2a675d05177175bede4d5e97ee30e9f6ad474174
alexboklag <sanya32102@gmail.com>
2019-12-27 12:30:38 +0200

    Add ControllerDetails

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { PublisherSubscriber } from './share/PublisherSubscriber.js';
 import { ControllerSearch } from './components/search/ControllerSearch.js';
 import { ControllerAnimals } from './components/animals/ControllerAnimals.js';
+import { ControllerDetails } from './components/details/ControllerDetails.js';
 
 const ps = new PublisherSubscriber();
 const search = new ControllerSearch(ps.api);
 const animals = new ControllerAnimals(ps.api);
+const details = new ControllerDetails(ps.api);
