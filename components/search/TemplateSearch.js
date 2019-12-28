ecb80497d825f8fc964e22df224a774f19cae7ff
alexboklag <sanya32102@gmail.com>
2019-12-28 09:34:09 +0200

    Add getFiltersTemplate method

--- components/search/TemplateSearch.js
+++ components/search/TemplateSearch.js
@@ -5,4 +5,13 @@ export class TemplateSearch {
           <input class="uk-search-input" type="search" placeholder="Search...">
         </form>`;
   }
+
+  static getFiltersTemplate() {
+    return `<ul class="uk-nav uk-nav-default uk-flex uk-flex-around">
+        <li data-value="cat">CATS</a></li>
+        <li data-value="dog">DOGS</li>
+        <li data-value="bird">BIRDS</li>
+        <li data-value="fish">FISHES</li>
+    </ul>`;
+  }
 }
