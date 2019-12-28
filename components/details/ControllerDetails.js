adee6ba9ae400ffd7dbf59823ebccbc3caf1dbaa
alexboklag <sanya32102@gmail.com>
2019-12-28 09:33:09 +0200

    Remove unwanted ModelDetails

--- components/details/ControllerDetails.js
+++ components/details/ControllerDetails.js
@@ -1,9 +1,7 @@
-import { ModelDetails } from './ModelDetails.js';
 import { ViewDetails } from './ViewDetails.js';
 
 export class ControllerDetails {
   constructor({ subscribe }) {
-    this.model = new ModelDetails();
     this.view = new ViewDetails(subscribe);
     subscribe('details-click', this.view.renderDetails.bind(this.view));
   }
