83a3bb8128f4fece46f0ebba9f5afad5c4ad048c
alexboklag <sanya32102@gmail.com>
2019-12-28 09:34:30 +0200

    Add handleChangeFilter method

--- components/search/ControllerSearch.js
+++ components/search/ControllerSearch.js
@@ -1,26 +1,43 @@
-import { ModelSearch } from './ModelSearch.js';
 import { ViewSearch } from './ViewSearch.js';
 
 export class ControllerSearch {
-  constructor({ subscribe, unsubscribe, publish }) {
+  constructor({ subscribe, publish }) {
     this.publish = publish;
-    this.model = new ModelSearch();
     this.view = new ViewSearch(subscribe);
-    this.view.renderSearch();
-    this.view.addListeners(this.handleChangeSearch.bind(this));
+    this.view.renderSearchAndFilters();
+    this.view.addListeners(this.handleChangeSearch.bind(this), this.handleChangeFilter.bind(this));
   }
 
   handleChangeSearch(ev) {
-    const searchValue = ev.target.value.toLowerCase().trim();
+    const search = ev.target.value.toLowerCase().trim();
 
     let animalsList = JSON.parse(localStorage.animalsList);
     localStorage.filteredAnimalsList = JSON.stringify(animalsList
-      .filter(animal => animal.breed.toLowerCase().includes(searchValue)));
+      .filter(animal => animal.breed.toLowerCase().includes(search)));
     
     localStorage.totalPages = Math.ceil(JSON.parse(localStorage.filteredAnimalsList).length / localStorage.pageSize);
     localStorage.offset = 0;
 
     this.publish('search-changed', JSON
+      .parse(localStorage.filteredAnimalsList)
+      .slice(localStorage.offset, Number(localStorage.offset) + Number(localStorage.pageSize))
+    );
+  }
+
+  handleChangeFilter(ev) {
+    const filter = ev.target.dataset.value.toLowerCase();
+
+    let animalsList = JSON.parse(localStorage.animalsList);
+    localStorage.filteredAnimalsList = JSON.stringify(animalsList
+      .filter(animal => {
+        console.log(filter, animal.species.toLowerCase());
+        return filter === animal.species.toLowerCase();
+      }));
+    
+    localStorage.totalPages = Math.ceil(JSON.parse(localStorage.filteredAnimalsList).length / localStorage.pageSize);
+    localStorage.offset = 0;
+
+    this.publish('filter-changed', JSON
       .parse(localStorage.filteredAnimalsList)
       .slice(localStorage.offset, Number(localStorage.offset) + Number(localStorage.pageSize))
     );
