44bd78f2e32316b7304b2ab765045bedacb5ec04
alexboklag <sanya32102@gmail.com>
2019-12-30 12:18:12 +0200

    Swap buttons

--- components/search/TemplateSearch.js
+++ components/search/TemplateSearch.js
@@ -2,17 +2,18 @@ export class TemplateSearch {
   static getSearchTemplate() {
     return `<form class="uk-search uk-search-default uk-margin-top uk-width-1-1">
           <span uk-search-icon></span>
-          <input class="uk-search-input" type="search" placeholder="Search...">
+          <input class="uk-search-input uk-border-rounded" type="search" placeholder="Search...">
         </form>`;
   }
 
   static getFiltersTemplate() {
-    return `<ul class="uk-nav uk-nav-default uk-flex uk-flex-around">
-        <li class="uk-button uk-button-default" data-value="all">ALL</a></li>
-        <li class="uk-button uk-button-default" data-value="cat">CATS</a></li>
-        <li class="uk-button uk-button-default" data-value="dog">DOGS</li>
-        <li class="uk-button uk-button-default" data-value="bird">BIRDS</li>
-        <li class="uk-button uk-button-default" data-value="fish">FISHES</li>
+    return `<ul class="filters uk-nav uk-nav-default uk-flex uk-flex-center uk-text-bold">
+        <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="all">ALL</li>
+        <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="cat">CATS</li>
+        <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="dog">DOGS</li>
+        <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="fish">FISHES</li>
+        <li class="uk-button uk-button-default uk-border-rounded" data-value="bird">BIRDS</li>
+        
     </ul>`;
   }
 }
