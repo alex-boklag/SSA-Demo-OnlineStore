b52e74392d792f7c1906ca14550e7ce1ec50a605
alexboklag <sanya32102@gmail.com>
2020-01-02 23:07:15 +0200

    Add sort button

--- components/search/TemplateSearch.js
+++ components/search/TemplateSearch.js
@@ -2,18 +2,26 @@ export class TemplateSearch {
   static getSearchTemplate() {
     return `<form class="uk-search uk-search-default uk-margin-top uk-width-1-1">
           <span uk-search-icon></span>
-          <input class="uk-search-input uk-border-rounded" type="search" placeholder="Search...">
+          <input class="search uk-search-input uk-border-rounded" type="search" placeholder="Search...">
         </form>`;
   }
 
-  static getFiltersTemplate() {
+  static getFiltersAndSortTemplate() {
     return `<ul class="filters uk-nav uk-nav-default uk-flex uk-flex-center uk-text-bold">
         <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="all">ALL</li>
         <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="cat">CATS</li>
         <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="dog">DOGS</li>
         <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="fish">FISHES</li>
-        <li class="uk-button uk-button-default uk-border-rounded" data-value="bird">BIRDS</li>
-        
+        <li class="uk-button uk-button-default uk-margin-small-right uk-border-rounded" data-value="bird">BIRDS</li>
+        <li class="uk-button uk-button-default uk-border-rounded" type="button">Sort by</li>
+          <div uk-dropdown>
+              <ul class="uk-nav uk-dropdown-nav">
+                <li class="sortBy priceAsc"><span uk-icon="icon: chevron-up"></span> Price</li>
+                <li class="sortBy priceDesc"><span uk-icon="icon: chevron-down"></span> Price</li>
+                <li class="sortBy ageAsc"><span uk-icon="icon: chevron-up"></span> Age</li> 
+                <li class="sortBy ageDesc"><span uk-icon="icon: chevron-down"></span> Age</li>
+              </ul>
+          </div>
     </ul>`;
   }
 }
